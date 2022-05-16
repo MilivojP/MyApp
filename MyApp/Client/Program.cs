ac1ec20b1172dcc22ba22ff5390015c137b12780
Mateo <Mateo@KORISNIK-PC>
2022-05-16 19:13:59 +0200

    Add project files.

---
+++ MyApp/Client/Program.cs
@@ -0,0 +1,21 @@
+global using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MyApp.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("MyApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MyApp.ServerAPI"));
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddApiAuthorization();
+
+await builder.Build().RunAsync();
---
+++ MyApp/Server/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyApp.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+        public string Email { get; set; }
+        public string Token { get; set; } 
+        public bool isAdmin { get; set; }
+
+    }
+}
